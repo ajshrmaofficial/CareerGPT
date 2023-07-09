41cce4315d613687a5a57e26cc05d7fcb1a9622e
Ajay Sharma <ajshrmaofficial@gmail.com>
2023-07-10 00:48:01 +0530

    fixed ui bug

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
 import "../css/App.css";
+import { useContext } from "react";
+import { LoggedInContext } from "../utility/LoggedInContext";
 
 const LandingPage = () => {
+  const {isLoggedIn, setIsLoggedIn} = useContext(LoggedInContext);
+
   return (
     <div className="app-header">
       <h1>CareerGPT</h1>
       <h2>AI Powered Career Guidance</h2>
       <h3>Powered by OpenAI's GPT-3</h3>
       <h3 className="linkH3">
-        <Link className="link" to={"/register"}>
+        {!isLoggedIn && <Link className="link" to={"/register"}>
           SignUp
-        </Link>
+        </Link>}
       </h3>
     </div>
   );
